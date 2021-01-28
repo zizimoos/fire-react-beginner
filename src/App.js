4d5f66c3f34804704f8ee7641ce4b056bb647499
zizimoos_iMac <zizimoos@gmail.com>
2021-01-28 22:28:56 +0900

    init 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Bora from "./components/Bora";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bora
+        zombie="string"
+        action={true}
+        banco={["hello", 1, 2, 3, "4"]}
+      ></Bora>
     </div>
   );
 }
